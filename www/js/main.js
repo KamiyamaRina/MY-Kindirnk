033ea3e52f309b83e9d9f726da4cab15d75fdc1d
KamiyamaRina <r-kamiyama@griffin-net.co.jp>
2020-06-12 14:44:19 +0900

    [一覧ページ]リストを登録データから呼び出し

--- www/js/main.js
+++ www/js/main.js
@@ -2,75 +2,6 @@
 // Page init event
 document.addEventListener('init', function(event) {
   var page = event.target;
-
-  
-  // ページの動き
-    // topから登録
-    if (page.matches('#top-page')) {
-      page.querySelector('#registerbutton').onclick = function() {
-        document.querySelector('#navigator').pushPage('page2.html');
-      };
-    } 
-    // topから結果
-    if (page.matches('#top-page')) {
-      page.querySelector('#kindbutton').onclick = function() {
-        document.querySelector('#navigator').pushPage('page4.html');
-      };
-    }
-
-    // topから一覧
-    if (page.matches('#top-page')) {
-      page.querySelector('#listbutton').onclick = function() {
-        document.querySelector('#navigator').pushPage('page3.html');
-      };
-    } 
-
-    // 登録から一覧
-    if (page.matches('#register-page')) {
-      page.querySelector('#sendbutton').onclick = function() {
-        document.querySelector('#navigator').replacePage('page3.html');
-      };
-    } 
-
-    // 一覧から詳細
-    if (page.matches('#list-page')) {
-      page.querySelector('#sake1').onclick = function() {
-        document.querySelector('#navigator').pushPage('page5.html');
-      };
-    } 
-
-    // 詳細から編集
-    if (page.matches('#detail-page')) {
-      page.querySelector('#editbutton').onclick = function() {
-        document.querySelector('#navigator').pushPage('page6.html');
-      };
-    } 
-
-    // 編集から一覧
-    if (page.matches('#edit-page')) {
-      page.querySelector('#sendbutton').onclick = function() {
-        document.querySelector('#navigator').replacePage('page3.html');
-      };
-    } 
-
-    // 一覧から登録
-    if (page.matches('#list-page')) {
-      page.querySelector('#registerbutton2').onclick = function() {
-        document.querySelector('#navigator').pushPage('page2.html');
-      };
-    } 
-    // 一覧からtop
-    if (page.matches('#list-page')) {
-      page.querySelector('#topbutton').onclick = function() {
-        document.querySelector('#navigator').resetToPage('top.html');
-      };
-    } 
-    // 結果からtop
-    if (page.matches('#kind-page')) {
-      page.querySelector('#topbutton').onclick = function() {
-        document.querySelector('#navigator').resetToPage('top.html');
-      };
-    } 
   
   // 登録ページ変化
   if (page.matches('#register-page')) {
@@ -355,13 +286,107 @@ document.addEventListener('init', function(event) {
           });
         }
   
-      });      
-  
-  
+      });
   
     }); 
 
   } 
+
+  // 一覧ページ
+  if (page.matches('#list-page')) {
+    $.getJSON("json/data.json", function(json){               // JSON取得
+      var dataList = json.data;
+
+      for (let index = 0; index < dataList.length; index++) {
+
+        var listItem = dataList[index];
+        
+        var list = '<ons-list-item id="sake" odifier="longdivider" tappable>' + '<div class="center">' + '<span class="list-item__title">' + listItem.name + '</span>' + '<span class="list-item__subtitle">' + listItem.genre + '</span>' + '</div>' + '</ons-list-item>'
+        
+        $('.lists').append(list);
+
+        
+      }
+      
+      // 一覧から詳細
+      page.querySelector('#sake').onclick = function() {
+        document.querySelector('#navigator').pushPage('page5.html');
+      };
+      
+    });
+
+  } 
+  
+  // ページの動き
+    // topから登録
+    if (page.matches('#top-page')) {
+      page.querySelector('#registerbutton').onclick = function() {
+        document.querySelector('#navigator').pushPage('page2.html');
+      };
+    } 
+    // topから結果
+    if (page.matches('#top-page')) {
+      page.querySelector('#kindbutton').onclick = function() {
+        document.querySelector('#navigator').pushPage('page4.html');
+      };
+    }
+
+    // topから一覧
+    if (page.matches('#top-page')) {
+      page.querySelector('#listbutton').onclick = function() {
+        document.querySelector('#navigator').pushPage('page3.html');
+      };
+    } 
+
+    // 登録から一覧
+    if (page.matches('#register-page')) {
+      page.querySelector('#sendbutton').onclick = function() {
+        document.querySelector('#navigator').replacePage('page3.html');
+      };
+    } 
+
+    // 一覧から詳細
+    // if (page.matches('#list-page')) {
+    //   page.querySelector('#sake').onclick = function() {
+    //     document.querySelector('#navigator').pushPage('page5.html');
+    //   };
+    // } 
+
+    // 詳細から編集
+    if (page.matches('#detail-page')) {
+      page.querySelector('#editbutton').onclick = function() {
+        document.querySelector('#navigator').pushPage('page6.html');
+      };
+    } 
+
+    // 編集から一覧
+    if (page.matches('#edit-page')) {
+      page.querySelector('#sendbutton').onclick = function() {
+        document.querySelector('#navigator').replacePage('page3.html');
+      };
+    } 
+
+    // 一覧から登録
+    if (page.matches('#list-page')) {
+      page.querySelector('#registerbutton2').onclick = function() {
+        document.querySelector('#navigator').pushPage('page2.html');
+      };
+    } 
+
+    // 一覧からtop
+    if (page.matches('#list-page')) {
+      page.querySelector('#topbutton').onclick = function() {
+        document.querySelector('#navigator').resetToPage('top.html');
+      };
+    } 
+
+    // 結果からtop
+    if (page.matches('#kind-page')) {
+      page.querySelector('#topbutton').onclick = function() {
+        document.querySelector('#navigator').resetToPage('top.html');
+      };
+    } 
+  
 
 });
 
